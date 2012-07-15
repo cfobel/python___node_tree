b920fa877a60a8a3db36f3de24b7a0bf477fd929
Christian Fobel <christian@fobel.net>
2012-07-15 19:03:28 -0400

    Add Node.get_tree(), NodeTree.insert_before/after
    Add node_tree_to dot() function.
    When run as a script, generate 3 dot files, corresponding to the state
    of a NodeTree before copies of sub-trees are inserted back into the
    tree.  The names of the 3 generated files are:
        00_original.dot
        01_insert_1_after_0.dot
        02_insert_15_after_14.dot
    
    The name of the file indicates the index of the root of the sub-tree
    that was copied, along with the location where the sub-tree copy was
    inserted.
    
    Using dot (a program that is part of the GraphViz suite), the files can
    be converted to PDF-rendered diagrams.
    
    e.g.,
        for f in *.dot ; do dot -T pdf -o $(basename $f .dot).pdf $f; done;

--- node_tree.py
+++ node_tree.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+from contextlib import closing
+import copy
 from pprint import pprint
 import cPickle as pickle
+
+from jinja2 import Template
+from path import path
 
 
 class Node(object):
@@ -9,6 +18,11 @@ class Node(object):
         self.parent = None
         self.item = item
         self.children = []
+
+    def insert_before(self, node):
+        node.parent = self.parent
+        position = node.parent.index(self)
+        node.parent.children.insert(position, node)
 
     def insert_after(self, node):
         node.parent = self.parent
@@ -21,6 +35,7 @@ class Node(object):
 
     def remove_node(self, node):
         self.children.remove(node)
+        node.parent = None
 
     def index(self, value):
         return self.children.index(value)
@@ -35,16 +50,37 @@ class Node(object):
         for child in self.children:
             yield child
 
+    def get_tree(self):
+        return NodeTree(self.copy())
+
+    def copy(self):
+        new_node = Node(item=self.item)
+        new_node.children = [child.copy() for child in self.children]
+        for child in new_node:
+            child.parent = new_node
+        return new_node
+
 
 class NodeTree(object):
-    def __init__(self):
+    def __init__(self, children=None):
         self.root = Node(None)
-        #self.root.connect('node-appended', self.on_node_appended)
-        #self.root.connect('node-inserted', self.on_node_appended)
-        #self.root.connect('node-removed', self.on_node_removed)
         self._reset_index()
+        if children is not None:
+            try:
+                iter(children)
+                if isinstance(children, Node):
+                    children = [children]
+            except TypeError:
+                children = [children]
+            for c in children:
+                self.append_node(c)
+
+    @property
+    def max_depth(self):
+        return self._max_depth
 
     def _reset_index(self):
+        self._max_depth = 0
         self._node_to_id_map = {}
         self._id_to_path_map = []
         self._node_to_path_map = {}
@@ -75,6 +111,7 @@ class NodeTree(object):
             yield self._index, tuple(self._counts[:self._depth + 1]), node
             self._index += 1
         self._depth += 1
+        self._max_depth = max(self._max_depth, self._depth)
         while len(self._counts) < self._depth + 1:
             self._counts.append(0)
         for child in node:
@@ -92,8 +129,8 @@ class NodeTree(object):
             return parent[node_path[0]]
 
     def __iter__(self):
-        for index, node_path, node in self._iter_children():
-            yield node_path, node
+        for i in range(len(self)):
+            yield self._id_to_path_map[i], self[i]
 
     def __getitem__(self, key):
         try:
@@ -112,25 +149,59 @@ class NodeTree(object):
             self.insert_after(self[sibling_path], node)
         else:
             self.append_child(self.root, node)
-        self.on_node_appended(node)
 
     def append_child(self, parent, node):
         parent.append_node(node)
+        self.on_node_appended(node)
 
-    def insert_after(self, sibling, node):
+    def _insert_relative(self, insert_path_func, sibling, node):
         sibling.insert_after(node)
         sibling_path = self._node_to_path_map[sibling]
-        insert_path = sibling_path[:-1] + tuple([sibling_path[-1] + 1])
-        self.on_node_inserted(insert_path, node)
+        self.on_node_inserted(insert_path_func(sibling_path), node)
+
+    def _insert_after_path(self, node_path):
+        return node_path[:-1] + tuple([node_path[-1] + 1])
+
+    def insert_before(self, sibling, node):
+        self._insert_relative(lambda x: x, sibling, node)
+
+    def insert_after(self, sibling, node):
+        self._insert_relative(self._insert_after_path, sibling, node)
 
     def remove(self, node):
         node_path = self._node_to_path_map[node]
         node.parent.remove_node(node)
-        node_tree = NodeTree()
-        node_tree.append_child(node_tree.root, node)
-        node_tree._reindex()
+        node_tree = node.get_tree()
         self.on_node_removed(node_path, node_tree)
         return node_tree
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+
+_node_tree_dot_template_str = '''\
+digraph G {
+    rankdir="LR";
+{{ edges }}
+}
+'''
+
+
+def node_tree_to_dot(node_tree):
+    template = Template(_node_tree_dot_template_str)
+    with closing(StringIO.StringIO()) as sio:
+        for node_path, node in node_tree:
+                print >> sio, '%s [ label=<<table border="0"><tr><td>%s:</td><td>%s</td></tr></table>> ];' % (
+                        node_tree._node_to_id_map[node],
+                                node_tree._node_to_id_map[node], node.item)
+                print >> sio, '    ' * (len(node_path) - 1),
+                try:
+                    if node.parent is not node_tree.root:
+                        print >> sio, '%s->%s;' % (node_tree._node_to_id_map[
+                                node.parent], node_tree._node_to_id_map[node])
+                except KeyError:
+                    print node.item, node.parent.item
+        return template.render(edges=sio.getvalue())
 
 
 if __name__ == '__main__':
@@ -160,6 +231,12 @@ if __name__ == '__main__':
             count += 1
     node_tree.append_node(Node(count))
     count += 1
-    pprint([i for i in node_tree._iter_children()])
+    path('00_original.dot').write_bytes(node_tree_to_dot(node_tree))
 
-    print pickle.dumps(node_tree)
+    sub_tree = node_tree[1].get_tree()
+    node_tree.insert_after(node_tree[0], copy.deepcopy(sub_tree)[0])
+    path('01_insert_1_after_0.dot').write_bytes(node_tree_to_dot(node_tree))
+
+    sub_tree = node_tree[15].get_tree()
+    node_tree.insert_after(node_tree[14], sub_tree[0])
+    path('02_insert_15_after_14.dot').write_bytes(node_tree_to_dot(node_tree))
