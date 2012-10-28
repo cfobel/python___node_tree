489b7ee513de03c91cc47834e1f1b640b9d620a5
Christian Fobel <christian@fobel.net>
2012-10-28 01:30:32 -0400

    Prettify console tree output

--- node_tree.py
+++ node_tree.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from contextlib import closing
 try:
     import cStringIO as StringIO
 except ImportError:
     import StringIO
 import logging
-from contextlib import closing
 import copy
-from pprint import pprint
-import cPickle as pickle
 
 from jinja2 import Template
 from path import path
@@ -29,6 +27,13 @@ class NodeTree(object):
                 children = [children]
             for c in children:
                 self.append_node(c)
+
+    def __str__(self):
+        with closing(StringIO.StringIO()) as s:
+            for i, (path, node) in enumerate(self):
+                print >> s, '[%2d]' % i, path, node
+            msg = s.getvalue()
+        return msg
 
     def ungroup(self, nodes):
         if not nodes:
@@ -325,35 +330,96 @@ if __name__ == '__main__':
     node_tree.append_node(Node(count))
     count += 1
     path('00_original.dot').write_bytes(node_tree_to_dot(node_tree))
+    tree_count = 0
+    print '''
+************************************************************************
+[%2d]   Original tree
+************************************************************************
+'''.strip() % tree_count
+    print node_tree
 
+    tree_count += 1
     sub_tree = node_tree[1].get_tree()
     node_tree.insert_after(node_tree[0], copy.deepcopy(sub_tree)[0])
     path('01_insert_1_after_0.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Insert 1 after 0
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     sub_tree = node_tree[15].get_tree()
     node_tree.insert_after(node_tree[14], sub_tree[0])
     path('02_insert_15_after_14.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Insert 15 after 14
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     node_tree.remove(node_tree[12])
     path('03_remove_12.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Remove 12
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     sibling = node_tree[9]
     sub_tree = node_tree.remove(node_tree[5])
     node_tree.insert_after(sibling, sub_tree[0])
     path('04_remove_5_insert_after_9.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Remove 5 insert after 9
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     other_tree = NodeTree([node.copy() for node in [node_tree[1], node_tree[5]]])
     node_tree.insert_before(node_tree[0], other_tree[0])
     path('05_copy_1_5_insert_before_0.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Copy 1 5 insert before 0
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     node_tree.group([node_tree[i] for i in [0, 4, 5]])
     path('06_group_0_4_5.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Group 0 4 5
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     node_tree.ungroup([node_tree[i] for i in [0]])
     path('07_ungroup_0.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Ungroup 0
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     node_tree.group([node_tree[i] for i in [0, 1, 2, 4, 5]])
     path('08_group_0_1_2_4_5.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Group 0 1 2 4 5
+************************************************************************''' % tree_count
+    print node_tree
 
+    tree_count += 1
     node_tree.ungroup([node_tree[i] for i in [0, 16]])
     path('09_ungroup_0_16.dot').write_bytes(node_tree_to_dot(node_tree))
+    print '''
+************************************************************************
+[%2d]   Ungroup 0 16
+************************************************************************''' % tree_count
+    print node_tree
